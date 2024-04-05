510e393234504d332d86892fdd237947c36f2f60
Thomas Caron <tc.caron91@gmail.com>
2024-04-05 14:50:24 +0200

    Aggregate by category fixed

--- sparkVolume/aggregate_by_category.py
+++ sparkVolume/aggregate_by_category.py
@@ -2,14 +2,14 @@ from pyspark.sql import SparkSession
 
 def main():
     spark = SparkSession.builder \
-        .appName("AggregationParTypesInteraction") \
+        .appName("AggregationParCategory") \
         .getOrCreate()
     
     spark.sparkContext.setLogLevel("WARN")
     
     df = spark.read.parquet("hdfs://namenode:9000/result.parquet")
     
-    aggregated_data = df.groupBy("contentCategory", "interactionType").count().orderBy("username")
+    aggregated_data = df.groupBy("contentCategory", "interactionType").count().orderBy("contentCategory")
     aggregated_data.show()
 
     spark.stop()
